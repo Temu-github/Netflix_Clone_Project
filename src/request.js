039f53aa300ae275b803b26a2f30233f2c27b4bb
rahel <raheldawit21@yahoo.com>
2021-12-23 12:14:31 -0600

    update

--- src/request.js
+++ src/request.js
@@ -1,11 +1,13 @@
-import React from 'react'
+const API_KEY = "7abc99ee77e8f993d57232eb99399ce8";
 
-function request() {
-    return (
-        <div>
-            <h>hello</h>
-        </div>
-    )
-}
-
-export default request
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
