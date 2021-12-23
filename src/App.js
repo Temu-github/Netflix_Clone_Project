2df681f4bd7c50b8cba344fbf3f7f36445e27757
TEMESGEN D DESTA <temesgend2014@gmail.com>
2021-12-23 10:37:08 -0600

    Delete files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Friends</h1>
-      <p>we are ready to do our netflix project</p>
+      
 
     </div>
   );
