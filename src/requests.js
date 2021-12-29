2aa45e3b8ca642c6fc1430407655fd34d19e988b
TEMESGEN D DESTA <temesgend2014@gmail.com>
2021-12-28 23:26:48 -0600

    Update

--- src/requests.js
+++ src/requests.js
@@ -1,13 +1,13 @@
-const API_KEY = "fea438d5d3957ad448c00c39850829ba";
+const REACT_APP_API_KEY= "fea438d5d3957ad448c00c39850829ba";
 
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTrending: `/trending/all/week?api_key=${REACT_APP_API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${REACT_APP_API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${REACT_APP_API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${REACT_APP_API_KEY}&with_genres=99`,
 };
 export default requests;
