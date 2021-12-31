028dbedab9e01de27f49a015d1e05fd8116b1376
TEMESGEN D DESTA <temesgend2014@gmail.com>
2021-12-30 22:21:16 -0600

    update line

--- src/requests.js
+++ src/requests.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_networks=213`,
